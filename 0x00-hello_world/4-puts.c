5a6e5752ba34510909bd705e6c8f4a5e5d943d06
Ajayi Sunday <ajayisunday7273@gmail.com>
2023-06-15 11:32:14 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 int main(void)
 {
 	/*initialising string*/
-	char str[] = '"Programming is like building a multilingual puzzle\n';
+	char str[] = "\"Programming is like building a multilingual puzzle\n";
 	/*Writing to Standout*/
 	puts(str);
 	return(0);
