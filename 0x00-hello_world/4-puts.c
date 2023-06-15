782e2fb2b0f58508316bbeffcbd1363cc04a4c9d
Ajayi Sunday <ajayisunday7273@gmail.com>
2023-06-15 12:25:32 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
-#include <stdio.h>
-/*
- * Function to use puts for main
- *You are not allowed to use printf
- *Your program should end with the value 0
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return : Always 0 (Success)
  */
 int main(void)
 {
-
-	char str[] = "\"Programming is like building a multilingual puzzle\n";
-
-	puts(str);
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
