475ee433e2ab4d0726e396b3961c59e30f43d1a5
Ajayi Sunday <ajayisunday7273@gmail.com>
2023-06-16 13:10:19 -0700

    that prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- A program that prints all digit numbers without using char
+ *
+ * Return: The program return 0 after execution
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n);
+
+	return (0);
+}
