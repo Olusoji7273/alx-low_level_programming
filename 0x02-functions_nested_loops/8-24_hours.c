16c561e91622e70610300a823fae9aadf5dca4b4
Ajayi Sunday <ajayisunday7273@gmail.com>
2023-06-21 20:16:32 -0700

    that prints every minute of the day of Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * jack_bauer- Function that print every minutes of the day
+ *
+ * Return: Return 0 at execustion
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			j++;
+		}
+		i++;
+	}
+
+}
