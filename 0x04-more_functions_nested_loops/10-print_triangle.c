ca51f9c1f2b20b55984f8f31e6bf9cb5e6a3d052
Ajayi Sunday <ajayisunday7273@gmail.com>
2023-06-23 04:43:46 -0700

    function that prints a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,6 +6,7 @@
  */
 void print_triangle(int size)
 {
+
 	if (size <= 0)
 	{
 		_putchar('\n');
@@ -14,13 +15,14 @@ void print_triangle(int size)
 	{
 		int i;
 		int j;
+
 		for (i = 1; i <= size; i++)
 		{
 			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
 			}
-			for (j = i; j <= i; j++)
+			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
