94f5e2e27f9bdff85ecaff92478b53623bccc0d4
Ajayi Sunday <ajayisunday7273@gmail.com>
2023-06-29 10:05:38 -0700

    function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,26 +10,22 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (str[i])
+	while (str[i] != '\0')
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
+		if (str[0] >= 97 && str[0] <= 122)
 		{
-			i++;
+			str[0] = str[0] - 32;
 		}
-		if (str[i - 1] == ' ' ||
-				str[i - 1] == '\t' ||
-				str[i - 1] == '\n' ||
-				str[i - 1] == ',' ||
-				str[i - 1] == '!' ||
-				str[i - 1] == '?' ||
-				str[i - 1] == '"' ||
-				str[i - 1] == '(' ||
-				str[i - 1] == ')' ||
-				str[i - 1] == '{' ||
-				str[i - 1] == '}' ||
-				i == 0)
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+				|| str[i - 1] == ',' || str[i - 1] == '!'
+				|| str[i - 1] == '?' ||	str[i - 1] == '"'
+				|| str[i - 1] == '(' ||	str[i - 1] == ')'
+				|| str[i - 1] == '{' ||	str[i - 1] == '}')
 		{
-			str[i] -= 32;
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
 		}
 		i++;
 	}
